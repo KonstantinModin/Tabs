2d13b3c84c3b2dee0ed106d4a51b74c316d0e67c
Konstantin Modin <45493372+KonstantinModin@users.noreply.github.com>
2018-12-20 15:16:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function showTabContent(b) {
 
 info.addEventListener('click', function (event) {
     let target = event.target;
-    if (target && target.classList.contains('.info-header-tab')) {
+    if (target && target.classList.contains('info-header-tab')) {
         for (let i = 0; i < tab.length; i++) {
             if (target == tab[i]) {
                 hideTabContent(0);
